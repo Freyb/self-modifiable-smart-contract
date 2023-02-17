5d0093e33b7dd466813b2a7bbeb55cfce67469fc
Giacomo Pizzamiglio <72966791+pizzmile@users.noreply.github.com>
2023-02-17 14:25:48 +0100

    Implementation of the contract as class

--- demos/Tezos/delegate.py
+++ demos/Tezos/delegate.py
@@ -1,27 +1,22 @@
 import smartpy as sp
 
-# Define the contract class
 class DelegableContract(sp.Contract):
     def __init__(self, owner, delegate):
-        self.init(owner = owner, delegate = delegate)
-    
-    # Define the "delegate" function, which allows the owner to change the delegate
+        self.init(owner=owner, delegate=delegate)
+
     @sp.entry_point
     def delegate(self, delegate):
-        # Check if the caller is the owner of the contract
         sp.verify(sp.sender == self.data.owner)
-        
-        # Update the delegate
         self.data.delegate = delegate
-        
-    # Define the "call" function, which is used to interact with the contract
+
     @sp.entry_point
     def call(self, params):
-        # Check if the caller is the delegate
         sp.verify(sp.sender == self.data.delegate)
-        
-        # Update the contract based on the parameters provided
         self.data.update(params)
 
+    def update(self, params):
+        self.data.new_storage = params.new_storage
+        self.data.new_code = params.new_code
+
 # Create an instance of the contract, with the specified owner and delegate addresses
-contract = DelegableContract
+contract = DelegableContract(sp.address("tz1..."), sp.address("tz2..."))
