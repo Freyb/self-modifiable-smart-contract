23916af5e61e65efaac3122d1e614901a0902d40
Giacomo Pizzamiglio <72966791+pizzmile@users.noreply.github.com>
2023-02-17 14:22:38 +0100

    Class update of the contract
    Implementation of the contract as a class
    Implementation of test for the update method

--- demos/Tezos/baking.py
+++ demos/Tezos/baking.py
@@ -1,8 +1,23 @@
-def update_smart_contract(params):
-    return {"storage": params["new_storage"], "code": params["new_code"]}
+import smartpy as sp
 
-def main(params):
-    if params.get("type") == "update":
-        return update_smart_contract(params)
-    else:
-        return {"error": "Unsupported action"}
+class UpdateSmartContract(sp.Contract):
+    def __init__(self):
+        self.init()
+
+    @sp.entry_point
+    def update(self, params):
+        sp.verify(params.type == "update", message="Unsupported action")
+        self.set_data(new_storage=params.new_storage, new_code=params.new_code)
+
+@sp.add_test(name="UpdateSmartContract")
+def test():
+    c = UpdateSmartContract()
+
+    # Test updating the storage and code
+    c.update(type="update", new_storage="new storage", new_code="new code")
+    sp.verify(c.data.new_storage == "new storage")
+    sp.verify(c.data.new_code == "new code")
+
+    # Test unsupported action
+    with sp.revert("Unsupported action"):
+        c.update(type="unsupported", new_storage="new storage", new_code="new code")
